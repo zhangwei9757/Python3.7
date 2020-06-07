c0513c224533aff0177bbeb53ba5c3f307b0b2b5
zhangwei <245370344@qq.com>
2020-06-07 08:06:39 +0800

    update1

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name="zhangwei_message",
+      version="1.0.0",
+      description="张伟的消息模块",
+      long_description="张伟的消息模块",
+      author="zhangwei",
+      author_email="245370344@qq.com",
+      url="暂无",
+      py_modules=["zhangwei.send_message", "zhangwei.receive_message"])
